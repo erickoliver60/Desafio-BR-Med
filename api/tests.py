2346f5c1171c61b80c855143b9d01d4e5f70d10a
Erick Oliveira <erick.oliver60@gmail.com>
2023-06-06 20:21:50 -0300

    Cria testes básicos

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from datetime import date
 
-# Create your tests here.
+class TestViews(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_index_view(self):
+        response = self.client.get('/cotacao/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_exchange_rate_view(self):
+        today = date.today().strftime('%Y-%m-%d')
+        response = self.client.get(f'/cotacao/BRL/{today}/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_exchange_rate_view_with_interval(self):
+        start_date = '2023-05-04'
+        end_date = '2023-05-07'
+        response = self.client.get(f'/cotacao/BRL/{start_date}/{end_date}/')
+        self.assertEqual(response.status_code, 200)
