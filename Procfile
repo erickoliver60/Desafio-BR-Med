f71acaeffe72c76cd212df2aaf06fa90b0183c7e
Erick Oliveira <erick.oliver60@gmail.com>
2023-06-07 04:37:39 -0300

    Cria configurações para deploy.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn cotacoes.wsgi --log-file -
--- cotacoes/settings.py
+++ cotacoes/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^x(6@ted&$bo=vs@w7*1@r^fh5bw9sc%8=als)n=!6n@v1&5&n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,7 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
