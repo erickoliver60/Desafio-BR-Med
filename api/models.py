d8652a04e8983ce1968fdf4e651b32def16659cf
Erick Oliveira <erick.oliver60@gmail.com>
2023-06-07 02:15:33 -0300

    Cria models.py que será usado para guardar as consultas da api.

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class CurrencyQuote(models.Model):
+    base_currency = models.CharField(max_length=3)
+    target_currency = models.CharField(max_length=3)
+    quote = models.DecimalField(max_digits=20, decimal_places=16)
+    date = models.DateField()
+
+    def __str__(self):
+        return f'{self.base_currency} to {self.target_currency} on {self.date}'
